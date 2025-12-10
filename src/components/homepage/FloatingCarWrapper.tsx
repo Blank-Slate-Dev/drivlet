53f60e794d072acb680ac4c153721524215b5475
Oakley Richards <osr9915@gmail.com>
2025-12-11 07:59:43 +1100

    Adjust FloatingCarWrapper positioning and image size

--- src/components/homepage/FloatingCarWrapper.tsx
+++ src/components/homepage/FloatingCarWrapper.tsx
@@ -12,13 +12,13 @@ interface FloatingCarWrapperProps {
 export default function FloatingCarWrapper({ children }: FloatingCarWrapperProps) {
   return (
     <div className="relative">
-      {/* Floating Toyota car image - positioned to overlap both sections */}
+      {/* Floating Toyota car image - positioned off to the left, not affecting content */}
       <motion.div
         initial={{ opacity: 0, x: -100 }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true, margin: '-100px' }}
         transition={{ duration: 0.8, ease: 'easeOut' }}
-        className="pointer-events-none absolute left-0 top-1/2 z-20 hidden -translate-y-1/2 lg:block"
+        className="pointer-events-none absolute -left-[150px] top-1/2 z-20 hidden -translate-y-1/2 lg:block xl:-left-[100px] 2xl:-left-[50px]"
         style={{ marginTop: '-40px' }}
       >
         <Image
@@ -26,7 +26,7 @@ export default function FloatingCarWrapper({ children }: FloatingCarWrapperProps
           alt="Toyota aerial view"
           width={500}
           height={350}
-          className="w-[380px] object-contain drop-shadow-2xl xl:w-[450px] 2xl:w-[500px]"
+          className="w-[380px] object-contain drop-shadow-2xl xl:w-[420px] 2xl:w-[450px]"
         />
       </motion.div>
 
