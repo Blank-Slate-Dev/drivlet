c3f4ec6da2cc33c879090a94362f4dc2fd923e8d
Oakley Richards <osr9915@gmail.com>
2025-12-11 07:42:59 +1100

    Refactor app/page.tsx

---
+++ src/components/homepage/index.ts
@@ -0,0 +1,14 @@
+// src/components/homepage/index.ts
+export { default as RegistrationPlate } from './RegistrationPlate';
+export { default as BookingModal } from './BookingModal';
+export { default as HeroSection } from './HeroSection';
+export { default as ValuePropsSection } from './ValuePropsSection';
+export { default as HowItWorksSection } from './HowItWorksSection';
+export { default as FloatingCarWrapper } from './FloatingCarWrapper';
+export { default as ServicesSection } from './ServicesSection';
+export { default as TestimonialsSection } from './TestimonialsSection';
+export { default as ContactCTASection } from './ContactCTASection';
+export { default as FAQSection } from './FAQSection';
+export { default as Footer } from './Footer';
+
+export type { StateCode } from './RegistrationPlate';
