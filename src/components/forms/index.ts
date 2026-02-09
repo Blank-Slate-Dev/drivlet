b1a0be0dd2058487df56eb2b729a76af803a261f
Oakley Richards <osr9915@gmail.com>
2026-02-09 14:56:28 +1100

    Create index.ts

---
+++ src/components/forms/index.ts
@@ -0,0 +1,4 @@
+export { default as SignaturePad } from "./SignaturePad";
+export { default as PickupConsentForm } from "./PickupConsentForm";
+export { default as ReturnConfirmationForm } from "./ReturnConfirmationForm";
+export { default as ClaimLodgementForm } from "./ClaimLodgementForm";
